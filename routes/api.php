0bf068746fefd0f23e0633e90f535230fcaae28d
belchushka <savvashu@gmail.com>
2020-12-17 12:48:37 +0300

    some fixes

--- routes/api.php
+++ routes/api.php
@@ -6,9 +6,8 @@ use Illuminate\Support\Facades\Route;
 
 Route::post("/login", [AuthorizationController::class, "login"]);
 Route::get("/info",[\App\Http\Controllers\Core\SystemController::class,"info"]);
-Route::post("/user/create",[RegistrationController::class,"store"]);
-Route::post("/user/create/excel", [RegistrationController::class, "storeExcel"]);
 
 Route::middleware('auth:api')->group(function () {
-
+    Route::post("/user/create",[RegistrationController::class,"store"]);
+    Route::post("/user/create/excel", [RegistrationController::class, "storeExcel"]);
 });
